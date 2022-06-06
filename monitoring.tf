ea18999bf2e9c04baff8b4647fa8d7e79f9dc532
DESKTOP-IPPL0N0\toshm <toshmamatov2000@gmail.com>
2022-06-06 18:30:12 -0400

    Subscription added to SNS, target is my email

--- monitoring.tf
+++ monitoring.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic_subscription" "ec2_sns_target" {
-  topic_arn = aws_sns_topic.EC2_STATE.topic_arn
+  topic_arn = aws_sns_topic.EC2_STATE.topic.arn
   protocol = "email"
   endpoint = "toshmamatov2000@gmial.com"
 }
